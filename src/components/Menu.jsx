4ca4aa6368dbe645619651033395c12f491a33c3
Kristian Slaatsveen <gjeddakbs@gmail.com>
2021-03-01 00:06:24 +0100

    fiks meny. fikset mobilvisning

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,7 +6,6 @@ return <div className="menu">
         <li><a href="#download">Download</a></li>
         <li><a href="#features">Features</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
     </ul>
 </div>
 
