ae3b974493396521d30d8822f7fa876d79e642e0
Kristian Slaatsveen <gjeddakbs@gmail.com>
2021-02-28 02:26:47 +0100

    en hel del jobb. Fikset nye giffer. Mobiloptimalisering. Endret en del tekst

--- src/components/AppSection.jsx
+++ src/components/AppSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import registerGif from "../registerframed.gif";
-import graphsGif from "../graphs.gif";
-import logGif from "../log.gif";
+import graphsGif from "../graphsframed.gif";
+import logGif from "../logframed.gif";
 
 function AppSection()  {
 return <section className="appsection" id="features">
@@ -18,12 +18,13 @@ return <section className="appsection" id="features">
     <div className="featuregif">
     <h3>Stats & Graphs</h3>
     <p>Keep track of your progress with the help of statistics and graphs.</p>
+    
     <img src={graphsGif} alt="gif of app"></img>
     
     </div>
     <div className="featuregif">
     <h3>Training log</h3>
-    <p>Each climb is visible in your training log </p>
+    <p>Look back at previous climbs in your training log </p>
     <img src={logGif} alt="gif of app"></img>
     
     </div>
